c3652ba1da23a819f98583d9f501fdd722e4d583
roppe546 <roppe546@gmail.com>
2015-12-16 18:11:39 +0100

    Add more stuff.

--- src/main/webapp/resources/js/chat.js
+++ src/main/webapp/resources/js/chat.js
@@ -3,13 +3,11 @@
  */
 var socket = io('http://130.229.149.4:3000');
 $('form').submit(function() {
-    socket.emit('chat message', {username : $('#username').val(), message :$('#m').val()});
-    $('#username').val('');
+    socket.emit('chat message', $('#u').val() + ": " + $('#m').val());
     $('#m').val('');
     return false;
 });
 
 socket.on('chat message', function(msg) {
-    Console.log(msg);
     $('#messages').append($('<li>').text(msg));
 });
