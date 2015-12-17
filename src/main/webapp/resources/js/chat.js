8d464fbfc819d81af850b9c683c588b5e54d961c
roppe546 <roppe546@gmail.com>
2015-12-17 11:35:55 +0100

    Use deployed server instead of test server.

--- src/main/webapp/resources/js/chat.js
+++ src/main/webapp/resources/js/chat.js
@@ -1,7 +1,7 @@
 /**
  * Created by robin on 16/12/15.
  */
-var socket = io('http://130.229.149.4:3000');
+var socket = io('http://130.237.84.200:3000');
 $('form').submit(function() {
     socket.emit('chat message', $('#u').val() + ": " + $('#m').val());
     $('#m').val('');
