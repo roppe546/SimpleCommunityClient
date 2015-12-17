152d011dbdb29111d81344469ea8c92fa310bfa5
Johan Pettersson <xsnudelx@hotmail.com>
2015-12-17 20:13:03 +0100

    implemented alpha sendPm

--- src/main/webapp/resources/js/chatvertx.js
+++ src/main/webapp/resources/js/chatvertx.js
@@ -5,17 +5,18 @@
 var bus = new EventBus("http://localhost:4000/eventbus");
 
 bus.onopen = function () {
-    bus.registerHandler("chat.to.server", function (err, msg) {
+    bus.registerHandler("chat.to.client", function (err, msg) {
         $('#m').append(msg.body + "\n");
     });
 };
 
 function send(event) {
     if (event.keyCode == 13 || event.which == 13) {
-        var message = "derp";
-        alert(message);
+
+        var message = $('#u').val() + ": " + $('#m').val();
+
         if (message.length > 0) {
-            bus.publish("chat.to.server", {"theMessage": message});
+            bus.publish("chat.to.server", { "username":  $('#u').val(), "theMessage": $('#m').val() } );
             $('#m').val("");
         }
     }
